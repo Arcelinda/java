fea01a3286ef05c4491730ad697936a15b75f4b3
Arcelinda <arcysantoa@gmail.com>
2022-06-21 23:18:49 -0100

    add aula array

---
+++ CursoJava/src/packageJava/Arrays01.java
@@ -0,0 +1,13 @@
+package packageJava;
+
+public class Arrays01 {
+    public static void main(String[] args) {
+        int [] idades = new int[3];
+        idades[0]= 21;
+        idades[1]= 30;
+        idades[2]= 20;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
--- CursoJava/src/packageJava/Aula05EstruturasDeRepeticao04Exercicios05.java
+++ CursoJava/src/packageJava/Aula05EstruturasDeRepeticao04Exercicios05.java
@@ -1,22 +1,19 @@
 package packageJava;
 
-public class Aula05EstruturasDeRepeticao04Exercicios {
+public class Aula05EstruturasDeRepeticao04Exercicios05 {
     public static void main(String[] args) {
         // Dado o valor de um carro, descobre em quantos ele pode ser parcelado
         // Condicao valorParcela >= 1000
         // i = parcela
         double valorTotal = 30000;
-        for (int parcela = 1; parcela <= valorTotal; parcela++){
-            double valorParcela = valorTotal/parcela;
+        for (int parcela=(int) valorTotal;parcela>=1; parcela--){
 
-                    if(valorParcela < 1000){
-                       // System.out.println("Parcela"+parcela+ "R$" +valorParcela);
-                    }else{
-                        continue;
-                    }
+            double valorParcela = valorTotal/parcela;
+            if(valorParcela < 1000){
+                continue;
+            }
+
             System.out.println("Parcela"+parcela+ "R$" +valorParcela);
         }
-
     }
 }
-
